fe923aa58a848fec470f8f5602586db0db12f8a9
adamcellanjones <adamcellanjones@gmail.com>
2024-06-14 14:57:11 +0100

    Completed code for pulling all titles, added function to return single game release date

--- scripts/build_dataset.py
+++ scripts/build_dataset.py
@@ -2,30 +2,38 @@ import requests
 import json
 import time
 import pandas as pd
+from datetime import datetime
 
 def main():
-    # TODO: Collect the full list of titles from SteamSpy API
-    for i in range(2):
+    date = datetime.today().strftime('%Y%m%d')
+    
+    # Collect the full list of titles with owners data from SteamSpy API
+    for i in range(74):
         print(f"Getting page {i}")
-        data = pull_steamspy_all(page=i)
+        data = get_steamspy_all(page=i)
         
         if i == 0:
             mode = "w"
             header = True
         else:
-            mode = "a'"
+            mode = "a"
             header = False
-            
-        data.to_csv("../data/external/all_titles.csv", mode=mode, header=header)
-        time.sleep(61)
 
-    # TODO: Collect release date from Steam API
+        data.to_csv("../data/external/all_titles_{date}.csv", mode=mode, header=header)
+        time.sleep(1)
+
+    # TODO: Collect release dates from Steam API
 
     # TODO: Collect tags from SteamSpy API
 
-def pull_steamspy_all(page):
+def get_steamspy_all(page):
     url = f"https://steamspy.com/api.php?request=all&page={page}"
     response = requests.get(url).json()
     return pd.DataFrame(response).T
+
+def get_game_date(appid):
+    url = f"https://store.steampowered.com/api/appdetails?appids={appid}"
+    response = requests.get(url).json()
+    return response[str(appid)]['data']['release_date']['date']
 
 main()
