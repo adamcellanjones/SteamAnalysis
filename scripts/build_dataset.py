0e9c865363e8e40258f89850025358a39e09c34c
adamcellanjones <adamcellanjones@gmail.com>
2024-06-16 15:21:03 +0100

    Added code to pull dates and tags in batches

--- scripts/build_dataset.py
+++ scripts/build_dataset.py
@@ -4,13 +4,16 @@ import time
 import pandas as pd
 from datetime import datetime
 import os
+import math
 
 def main():
     date = datetime.today().strftime('%Y%m%d')
-    datapath = f"../data/external/all_titles_{date}.csv"
+    list_datapath = f"../data/external/all_titles_{date}.csv"
+    dates_datapath = "../data/external/game_dates.csv"
+    tags_datapath = "../data/external/game_tags.csv"
     
     # Collect the full list of titles with owners data from SteamSpy API
-    if not os.path.isfile(datapath):
+    if not os.path.isfile(list_datapath):
         for i in range(74):
             print(f"Getting page {i}")
             data = get_steamspy_all(page=i)
@@ -23,20 +26,54 @@ def main():
                 mode = "a"
                 header = False
 
-            data.to_csv(datapath, mode=mode, header=header)
+            data.to_csv(list_datapath, mode=mode, header=header)
             time.sleep(1)
 
-    # TODO: Collect release dates from Steam API
-    games_list_data = pd.read_csv(datapath).set_index("appid")
-    games_list = games_list_data.index
-    
-    batch = games_list[:100]
-    batch_dates = get_game_dates(batch)
+    # Collect release dates from Steam API and genres and tags from SteamSpy API
+    games_list = pd.read_csv(list_datapath).set_index("appid").index
 
-    batch_dates.to_csv("../data/external/game_dates.csv", header=True)
+    # Check if we already have some data - if yes, exclude games from list and append data, if not start new file
+    if os.path.isfile(dates_datapath):
+        games_with_dates = pd.read_csv(dates_datapath).set_index("appid").index
+        dates_mode = "a"
+        dates_header = False
+    else:
+        games_with_dates = []
+        dates_mode = "w"
+        dates_header = True
+    if os.path.isfile(tags_datapath):
+        games_with_tags = pd.read_csv(tags_datapath).set_index("appid").index
+        tags_mode = "a"
+        tags_header = False
+    else:
+        games_with_tags = []
+        tags_mode = "w"
+        tags_header = True
 
+    games_list_to_pull = [id for id in games_list if 
+                          id not in set(games_with_dates) or 
+                          id not in set(games_with_tags)]
 
-    # TODO: Collect tags from SteamSpy API
+    # Work in batches of 50
+    start_point = 0
+    batch_size = 50
+    batches = math.ceil(len(games_list_to_pull)/batch_size)
+    i = 0
+    while start_point < len(games_list_to_pull):
+        end_point = start_point + batch_size
+        batch = games_list_to_pull[start_point:end_point]
+        
+        batch_dates = get_game_dates(batch)
+        batch_dates.to_csv("../data/external/game_dates.csv", header=dates_header, mode = dates_mode)
+
+        batch_tags = get_game_tags(batch)
+        batch_tags.to_csv("../data/external/game_tags.csv", header=tags_header, mode = tags_mode)
+
+        start_point = end_point
+        i+=1
+
+        print(f"Batch {i}/{batches} complete.")
+
 
 def get_steamspy_all(page):
     """
@@ -79,7 +116,7 @@ def get_game_dates(appid_list):
     return pd.DataFrame(zip(appid_list, dates), columns=["appid", "date"])
 
 
-def get_game_genre_tags(appid_list, tag_limit=5):
+def get_game_tags(appid_list, tag_limit=5):
     """
     This function returns a Pandas dataframe of genres and tags for a given list of app IDs.
 
@@ -88,25 +125,25 @@ def get_game_genre_tags(appid_list, tag_limit=5):
     tag_limit (int): the number of tags to include for each game
     """
     n_games = len(appid_list)
-    genre_tags_data = []
+    tags_data = []
 
     for i, appid in enumerate(appid_list):     
         print(f"Getting genre/tags for game {appid} ({i+1}/{n_games})")
         url = f"https://steamspy.com/api.php?request=appdetails&appid={appid}"
         response = requests.get(url).json()
 
-        game_genre_tags = {"appid": appid, "genre": response['genre']}
+        game_tags = {"appid": appid, "genre": response['genre']}
         for i, tag in enumerate(response['tags']):
             if i == tag_limit:
                 break
-            game_genre_tags.update({f"tag{i+1}": tag})
+            game_tags.update({f"tag{i+1}": tag})
         
-        genre_tags_data.append(game_genre_tags)
+        tags_data.append(game_tags)
 
         # Rate limit of 1 call per second
         time.sleep(1)
     
-    return pd.DataFrame(genre_tags_data)
+    return pd.DataFrame(tags_data)
 
 
 main()
